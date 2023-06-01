51c768f80e68684bef5ca978744305e14ab20045
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-01 10:16:26 +0000

    Added image_filter choice and validation

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     
     #own
     'profiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
---
+++ posts/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from posts.models import Post
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    
+    # validate is django rest built in feature, after underscore is name of variable from the model Post
+    # if we follow naming convention it will be called automatically when we create or update the post
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2: # 2 MB
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        return value
+    
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+    
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'owner',
+            'is_owner',
+            'profile_id',
+            'profile_image',
+            'created_at',
+            'updated_at',
+            'title',
+            'content',
+            'image',
+            'image_filter',
+        ]
