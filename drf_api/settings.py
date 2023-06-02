81ebb9e7276e36cbadd221ca11ed76f74fa78246
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-02 19:34:57 +0000

    change config

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -109,29 +109,24 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# in development
-if 'DEV' in os.environ:
-    ALLOWED_HOSTS = [
-        os.environ.get('ALLOWED_HOST'),
-        'localhost'
-    ]
-# in production     
-else:
-    ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
-    
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
+
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
-
+    
+CORS_ALLOW_CREDENTIALS = True
+    
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
