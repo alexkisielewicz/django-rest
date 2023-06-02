086cef1cec252576db5aa47d24177a6bfc11a989
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-02 17:12:50 +0000

    Change CORS settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -120,10 +120,12 @@ if 'CLIENT_ORIGIN' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+        r"^http://localhost:3000$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^https://.*\.gitpod\.io$",
+        r"^http://localhost:3000$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
