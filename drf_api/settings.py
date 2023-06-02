c5d36005d13ac1765585edb16f27ccf4856626bc
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-02 11:36:16 +0000

    Add client origin dev settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -64,10 +65,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1",
-                 "localhost",
-                 "drfapi-ci.herokuapp.com"
-                 ]
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    '127.0.0.1',
+    'localhost',
+    ]
 
 
 # Application definition
@@ -115,8 +117,9 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
